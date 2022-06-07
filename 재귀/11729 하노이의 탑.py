3ba754f18ab00e7c212eedfa4e054a2d21b5489b
zkgkthsus <zkgkthsus@naver.com>
2022-06-08 00:42:21 +0900

    11729 여전히 헤깔림

--- 재귀/11729 하노이의 탑.py
+++ 재귀/11729 하노이의 탑.py
@@ -1,19 +1,21 @@
 a = int(input())
-L = [i for i in range(a, 1, -1)]
-C = []
-R = []
-pro = []
 
 
-def recursion(L, C, R):
-    if len(L) > 0:
-        if len(C) == 0:
-            C.append(L.pop())
+def hanoi(x, start, end):
+    if x == 1:
+        print(start, end)
+        return
+    hanoi(x - 1, start, 6 - start - end)
+    print(start, end)
+    hanoi(x - 1, 6 - start - end, end)
 
 
-if a % 2 == 1:
-    pro.append([1, 3])
-    R.append(1)
-elif a % 2 == 0:
-    pro.append([1, 2])
-    C.append(1)
+# 막대가 1개 일 경우 이동횟수는 1 = 2 ** 1 -1
+# 막대가 2개 일 경우 이동횟수는 3 = 2 ** 2 -1
+# 막대가 3개 일 경우 이동횟수는 7 = 2 ** 3 -1
+# 막대가 4개 일 경우 이동횟수는 15 = 2 ** 4 -1
+# 따라서 횟수에 대한 공식은 2**n -1이다
+print(2 ** a - 1)
+hanoi(a, 1, 3)
+
+
