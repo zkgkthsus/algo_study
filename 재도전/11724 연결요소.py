78244b601e5d698b62a4cab59f28f64b51782b68
zkgkthsus <zkgkthsus@naver.com>
2022-06-25 22:31:37 +0900

    11724 시간초과로 구조 변경중 2

--- 재도전/11724 연결요소.py
+++ 재도전/11724 연결요소.py
@@ -1,20 +1,27 @@
-m, n = map(int,input().split())
-check = [i for i in range(1, m + 1)]
-def team_set(y, x):
-    if y > x:
-        for i in range(m):
-            if (x - 1) != i and check[i] == check[y - 1]:
-                team_change(i, x)
-    elif y < x:
-        for i in range(m):
-            if (y - 1) != i and check[i] == check[x - 1]:
-                team_change(i, y)
+# 재귀 제한 때문에 아래 두줄은 필수
+import sys
+sys.setrecursionlimit(10000)
 
-def team_change(i, y):
-    check[i] = check[y - 1]
+def dfs(v):
+    visit[v] = True
+    for i in check[v]:
+        if not visit[i]:
+            dfs(i)
+
+m, n = map(int,input().split())
+check = [[] for i in range(m + 1)]
+visit = [False] * (m + 1)
+cnt = 0
+
 
 for i in range(n):
     y, x = map(int,input().split())
-    team_set(y,x)
-check = set(check)
-print(len(check))
+    check[y].append(x)
+    check[x].append(y)
+
+for j in range(1, m + 1):
+    if not visit[j]:
+        dfs(j)
+        cnt += 1
+
+print(cnt)
